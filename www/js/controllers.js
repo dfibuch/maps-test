e6a370fc3f23055ff677631482ed1790f1d8189d
Tom Hyde <thyde@neworbit.co.uk>
2017-03-24 16:51:01 +0000

    A map is displayed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,6 +3,9 @@ angular.module('starter.controllers', [])
 .controller('MapCtrl', function($scope, $ionicPlatform) {
   $ionicPlatform.ready().then(() => {
     let mapElement = document.getElementById('map');
-    window.plugin.google.maps.Map.getMap(mapElement, {});
+    let mapOptions = {
+      mapType: plugin.google.maps.MapTypeId.HYBRID
+    };
+    window.plugin.google.maps.Map.getMap(mapElement, mapOptions);
   });
 });
